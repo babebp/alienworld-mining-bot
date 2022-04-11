3880e6656b4b2d27e7cd018a97957eda595c4c54
Pacharawut Thanawut <tpacharawut@hotmail.com>
2022-04-11 17:56:06 +0700

    finish approve method

--- modules/mine.py
+++ modules/mine.py
@@ -1,6 +1,11 @@
 import pickle
 import time
+from datetime import datetime
+from termcolor import cprint
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 
 class AlienWorld:
     def __init__(self, ):
@@ -13,4 +18,16 @@ class AlienWorld:
         self.driver.get("https://play.alienworlds.io/")
 
     def approve(self):
-        pass
+        try:
+            time.sleep(5)
+            self.driver.switch_to.window(self.driver.window_handles[1])
+            WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/section/div[2]/div/div[5]/button/div")))
+            self.driver.find_element(by=By.XPATH, value='/html/body/div/div/section/div[2]/div/div[5]/button/div').click()
+            cprint(f'({datetime.now().strftime("%H:%M:%S")}) Approve Success', 'white', on_color='on_green')
+            self.driver.switch_to.window(self.driver.window_handles[0])
+
+        except:
+            self.driver.switch_to.window(self.driver.window_handles[1])
+            self.driver.close()
+            self.driver.switch_to.window(self.driver.window_handles[0])
+            cprint(f'({datetime.now().strftime("%H:%M:%S")}) Cant Approve', 'white', on_color='on_red')
