23cd275ec5e000479c89e9f5f9fdc15293cf8784
Pacharawut Thanawut <tpacharawut@hotmail.com>
2022-04-11 15:58:04 +0700

    LOGIN METHOD

---
+++ main.py
@@ -0,0 +1,5 @@
+from modules.mine import AlienWorld
+
+if __name__ == "__main__":
+    alien = AlienWorld()
+    alien.login('XrWgdS2YrTHTcNQbTUw3eRjlAKNQNXZoOPthzDiu')
--- modules/mine.py
+++ modules/mine.py
@@ -1,3 +1,13 @@
+import pickle
+import time
+from selenium import webdriver
+
 class AlienWorld:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, ):
+        self.driver = webdriver.Chrome(executable_path=r"C:\Users\user\Projects\main_project\aw_bot\chromedriver.exe")
+        
+    def login(self, session):    
+        self.driver.get("https://all-access.wax.io/")
+        time.sleep(10)
+        self.driver.add_cookie({"name":"session_token","value":f"{session}"})
+        self.driver.get("https://play.alienworlds.io/")
