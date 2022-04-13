a9f6bb3aa5b4c25f489396391b9ce04d97b1f9d8
Pacharawut Thanawut <tpacharawut@hotmail.com>
2022-04-14 00:26:53 +0700

    remove while True method

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from modules.mine import AlienWorld
-import random
 import time
 
 if __name__ == "__main__":
     alien = AlienWorld()
     session_token = input('Enter Your Session Token: ')
     alien.login(session_token)
-    while True:
-        time.sleep(240 + random.randint(10,60))
+
+    time.sleep(5)
+    alien.all_in_one()
