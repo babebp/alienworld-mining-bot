c0c9a19f619d4ce5112a30337c3b8f8645495c0d
Pacharawut Thanawut <tpacharawut@hotmail.com>
2022-04-14 00:26:38 +0700

    combine method to all_in_one

--- modules/mine.py
+++ modules/mine.py
@@ -1,17 +1,20 @@
-import pickle
-from ssl import Options
-import time
+import random
+import time  
+
 from datetime import datetime
 from termcolor import cprint
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.common.exceptions import NoSuchElementException    
 from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+
+
+
 
 class AlienWorld:
     options = webdriver.ChromeOptions()
-    options.add_extension()
     def __init__(self, ):
         self.driver = webdriver.Chrome(executable_path=r"C:\Users\user\Projects\main_project\aw_bot_\chromedriver.exe")
         
@@ -31,22 +34,44 @@ class AlienWorld:
             self.driver.switch_to.window(self.driver.window_handles[1])
             WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div/div/section/div[2]/div/div[5]/button/div")))
             self.driver.find_element(by=By.XPATH, value='/html/body/div/div/section/div[2]/div/div[5]/button/div').click()
-            cprint(f'({datetime.now().strftime("%H:%M:%S")}) Approve Success', 'green')
             self.driver.switch_to.window(self.driver.window_handles[0])
 
         except:
             self.driver.switch_to.window(self.driver.window_handles[1])
             self.driver.close()
             self.driver.switch_to.window(self.driver.window_handles[0])
-            cprint(f'({datetime.now().strftime("%H:%M:%S")}) Cant Approve', 'red')
 
-    def click_mine(self):
+    def all_in_one(self):
+        while True:
+            # Click Mine
+            try:
+                WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[3]/div[1]/div/div[3]/div[5]/div[2]/div/div/div/div/div/div/span")))
+                self.driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[3]/div[1]/div/div[3]/div[5]/div[2]/div/div/div/div/div/div/span').click()
+            except:
+                pass
 
-        # Click Mine
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[3]/div[2]/div[1]/div/div/div[2]/div/div/div[4]/div[3]/div[2]/div[2]/div/div/div/div/div/div/span")))
-        self.driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[3]/div[2]/div[1]/div/div/div[2]/div/div/div[4]/div[3]/div[2]/div[2]/div/div/div/div/div/div/span').click()
+            time.sleep(4)
 
-        time.sleep(5)
-        # Click Claim Mine
-        WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[3]/div[2]/div[1]/div/div/div[2]/div/div/div[4]/div[3]/div[2]/div[2]/div/div/div/div/div/span")))
-        self.driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[3]/div[2]/div[1]/div/div/div[2]/div/div/div[4]/div[3]/div[2]/div[2]/div/div/div/div/div/span').click()
+            try:
+                WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[3]/div[1]/div/div[3]/div[5]/div[2]/div/div")))
+                self.driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[3]/div[1]/div/div[3]/div[5]/div[2]/div/div').click()
+            except:
+                pass
+            
+            try:
+                self.approve()
+            except:
+                pass
+
+            try:
+                WebDriverWait(self.driver, 10).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[1]/div/div/div[2]")))
+                self.driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[1]/div/div/div[2]').click()
+
+                cprint(f"({datetime.now().strftime('%H:%M:%S')}) Get => {self.driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[1]/div/div/div[2]').text}", 'green', attrs=['bold'])
+                cprint(f"({datetime.now().strftime('%H:%M:%S')}) CPU : {self.driver.find_element(by=By.XPATH, value='/html/body/div[1]/div[3]/div[2]/div[1]/div/div/div[2]/div/div/div[10]/div[1]/div[1]/p[2]').text}", 'green', attrs=['bold'])
+
+            except:
+                pass
+
+            print('----------------------------')
+            time.sleep(random.randint(10,60))
