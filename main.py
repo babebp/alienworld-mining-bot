7e187a18880f6b06b14758931661e3fa984911ed
Pacharawut Thanawut <tpacharawut@hotmail.com>
2022-04-28 07:04:08 +0700

    auto add session_token

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import time
 
 if __name__ == "__main__":
     alien = AlienWorld()
-    session_token = input('Enter Your Session Token: ')
-    alien.login(session_token)
+
+    with open('session_token.txt') as file:
+        lines = file.readlines()
+
+    for i in range(len(lines)):
+        session_token = lines[i]
+        alien.login(session_token.strip())
 
     time.sleep(5)
     alien.all_in_one()
